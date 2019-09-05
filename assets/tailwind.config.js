95fc56a0405c668d9a6b25dd427a0b7485515339
Paweł Świątkowski <katafrakt@vivaldi.net>
2019-09-05 20:16:46 +0200

    Replace standard Phoenix CSS with Tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        lighty: '#B1B695',
+        bluish: '#171738',
+        bground: '#CBD4C2'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
